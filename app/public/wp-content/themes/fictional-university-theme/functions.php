4e47e9f8f237b6c725efa4dbc2fd72f1bd6cc663
noorulain-paracha <ulain80@gmail.com>
2021-05-03 13:33:17 +0500

    Navigation menus

--- app/public/wp-content/themes/fictional-university-theme/functions.php
+++ app/public/wp-content/themes/fictional-university-theme/functions.php
@@ -15,8 +15,10 @@ add_action('wp_enqueue_scripts','university_files');
 
 /* Adding title of the browser tab*/
 function university_features() {
-    add_theme_support('title-tag');
+   add_theme_support('title-tag');
 }
 
 add_action('after_setup_theme','university_features');
+
+
 ?>
