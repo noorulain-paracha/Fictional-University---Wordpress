e858122d4ac04d83b0b41eb669d784abe2e0aa3a
noorulain-paracha <ulain80@gmail.com>
2021-05-04 13:48:34 +0500

    Custom Post Types

--- app/public/wp-content/themes/fictional-university-theme/js/modules/MobileMenu.js
+++ app/public/wp-content/themes/fictional-university-theme/js/modules/MobileMenu.js
@@ -1,5 +1,6 @@
 class MobileMenu {
   constructor() {
+
     this.menu = document.querySelector(".site-header__menu")
     this.openButton = document.querySelector(".site-header__menu-trigger")
     this.events()
