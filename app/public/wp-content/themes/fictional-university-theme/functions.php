8e56ce4979503e281d83248a4a82454c08d3de4d
noorulain-paracha <ulain80@gmail.com>
2021-05-19 04:25:24 +0500

    Synchronous VS Asynchronous

--- app/public/wp-content/themes/fictional-university-theme/functions.php
+++ app/public/wp-content/themes/fictional-university-theme/functions.php
@@ -47,8 +47,8 @@ function university_files() {
     } else {
         
     wp_enqueue_script('over-vendors-js', get_theme_file_uri('/bundled-assets/vendors~scripts.1fa169383e64a33bfd0c.js'),NULL,'1.0',true);
-    wp_enqueue_script('main-university-js', get_theme_file_uri('/bundled-assets/scripts.7be82861cd3414529b43.js'),NULL,'1.0',true);
-    wp_enqueue_style('our-main-styles', get_theme_file_uri('/bundled-assets/styles.7be82861cd3414529b43.css'));
+    wp_enqueue_script('main-university-js', get_theme_file_uri('/bundled-assets/scripts.84a4e93cd7700819b960.js'),NULL,'1.0',true);
+    wp_enqueue_style('our-main-styles', get_theme_file_uri('/bundled-assets/styles.84a4e93cd7700819b960.css'));
     }
 
     wp_localize_script('main-university-js', 'universityData', array(
