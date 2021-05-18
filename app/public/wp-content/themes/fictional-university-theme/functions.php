6b80fa7a0b319c10a78df4e3da574b687ada02bf
noorulain-paracha <ulain80@gmail.com>
2021-05-18 21:26:44 +0500

    Featured Image (post Thumbnail)

--- app/public/wp-content/themes/fictional-university-theme/functions.php
+++ app/public/wp-content/themes/fictional-university-theme/functions.php
@@ -26,6 +26,7 @@ add_action('wp_enqueue_scripts','university_files');
 /* Adding title of the browser tab*/
 function university_features() {
    add_theme_support('title-tag');
+   add_theme_support('post-thumbnails');
 }
 
 add_action('after_setup_theme','university_features');
